7420d907d0e150f9333f9071cd84db0255720b27
jeffersonccomp <jefferson_cc2012@hotmail.com>
2021-11-25 21:13:39 -0300

    atualizacao pasta

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,5 +1,6 @@
+package application;
 public class Programa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ola mundo ");
     }
 }
