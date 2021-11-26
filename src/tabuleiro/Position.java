bf0ca344635f6047310e2ba8fc66b965b98d2df7
jeffersonccomp <jefferson_cc2012@hotmail.com>
2021-11-25 21:28:14 -0300

    position

---
+++ src/tabuleiro/Position.java
@@ -0,0 +1,26 @@
+package tabuleiro;
+
+public class Position {
+    
+    private int row;
+    private int collum;
+
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCollum() {
+        return this.collum;
+    }
+
+    public void setCollum(int collum) {
+        this.collum = collum;
+    }
+
+
+}
